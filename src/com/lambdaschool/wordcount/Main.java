9d480e7ee17d41afe43dde417507d6e5b4be0045
JasonCorp84 <csababalogh84@gmail.com>
2019-02-05 15:20:38 -0800

    50 most used word listed, assignment done

--- src/com/lambdaschool/wordcount/Main.java
+++ src/com/lambdaschool/wordcount/Main.java
@@ -5,7 +5,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
@@ -19,16 +21,47 @@ public class Main {
 
         String line = reader.readLine();
         while(line != null) {
-            System.out.println("Processing line: " + line);
+            if(!line.trim().equals("")) {
+                String [] words = line.split(" ");
+                for(String word: words)
+                {
+                    if(word == null || word.trim().equals("")){
+                        continue;
+                    }
+                    String processed = word.toLowerCase();
+                    if(frequency.containsKey(processed)) {
+                        frequency.put(
+                                processed,
+                                frequency.get(processed) + 1
+                        );
+                    } else {
+                        frequency.put(processed, 1);
+                    }
+                }
+            }
+            line = reader.readLine();
+        }
+        System.out.println(frequency);
 
-//            if(!line.trim().equals("")) {
-//                String [] words = line.split(" ");
-//            }
 
+        int removeCounter = 1;
+        while(removeCounter < 51)
+        {
+            int mostFrequent = 0;
+            String theWord = null;
+            for (String word : frequency.keySet())
+            {
+                Integer theVal = frequency.get(word);
+                if (theVal > mostFrequent)
+                {
+                    mostFrequent = theVal;
+                    theWord = word;
+                }
+            }
+            System.out.println("(" + removeCounter + ") The most frequent word is: '" + theWord + "' appears: " + mostFrequent);
 
-
-            String [] words = line.split(" ");
-
+            frequency.remove(theWord);
+            removeCounter++;
         }
     }
 }
