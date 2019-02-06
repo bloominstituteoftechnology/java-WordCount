81d4bd01b9ec10ae93ed34cd54d4b84ca9b770b4
emaquino44 <prexy44@yahoo.com>
2019-02-05 17:19:30 -0800

    redo on files - tested main.java

--- src/WordCountApp/main.java
+++
@@ -1,8 +0,0 @@
-package WordCountApp;
-
-public class Main {
-   public static void main(String[] args)
-   {
-       System.out.println("*** Arrays ***");
-   }
-}
---
+++ src/com/LambdaSchool/Main.java
@@ -0,0 +1,8 @@
+package com.LambdaSchool;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
