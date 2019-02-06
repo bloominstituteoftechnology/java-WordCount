951588e4f3435663cf80748e76a970eb5edd3280
habib1234731 <habibrehman1234731@gmail.com>
2019-02-05 23:34:43 -0500

    created the hashmap and the loop which will count the words

--- src/com/lambdaschool/Main.java
+++ src/com/lambdaschool/Main.java
@@ -9,7 +9,22 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         File myfile = new File("myinput.txt");
-        Scanner inputFile = new Scanner(myFile);
-        
+        Scanner inputFile = new Scanner(myfile);
+
+        String str = inputFile.nextLine();
+        str.replaceAll("[[\\.\\?\\!\\,\\;\\:\\{\\}\\(\\)\\']]", "");
+        String[] words = str.split(" +");
+
+        HashMap <String, Integer> wordCountHashMap = new HashMap<>();
+
+        for (String word:words) {
+            if (!wordCountHashMap.containsKey(word)) {
+                wordCountHashMap.put(word, 1);
+            } else {
+                int count = wordCountHashMap.get(word);
+                wordCountHashMap.put(word, count + 1);
+            }
+        }
+        System.out.println("loop compiled successfully");
     }
 }
