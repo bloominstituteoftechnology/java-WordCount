85ccbcb24dbd0e6b710f0d660d054a9f17fe7609
habib1234731 <habibrehman1234731@gmail.com>
2019-02-05 23:11:50 -0500

    initialized the project

---
+++ src/com/lambdaschool/Main.java
@@ -0,0 +1,15 @@
+package com.lambdaschool;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        File myfile = new File("myinput.txt");
+        Scanner inputFile = new Scanner(myFile);
+        
+    }
+}
