f7b9ff5bf0a169060286df90a40a7a16d27bf490
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-02-05 16:38:51 -0800

    Initial file setup

--- src/com/lambdaschool/Main.java
+++ src/com/lambdaschool/Main.java
@@ -1,8 +1,10 @@
 package com.lambdaschool;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        // write your code here
     }
 }
