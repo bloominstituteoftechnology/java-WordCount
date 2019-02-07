f878668b242d17f8cd096d8333d9dc771d2e8f0b
johnoro <officialjohnorourke@gmail.com>
2019-02-07 13:41:33 -0500

    Initial organization done, all that's left is making it work

--- src/wordCount/Main.java
+++ src/wordCount/Main.java
@@ -1,8 +1,18 @@
 package wordCount;
 
 public class Main {
-  private static void countWords() {
+  private static String readFile(String path, Charset encoding) throws IOException {
+    byte[] encoded = Files.readAllBytes(Paths.get(path));
+    return new String(encoded, encoding);
+  }
 
+  private static void countWords() {
+    String declaration = readFile("../../declaration.txt", StandardCharsets.UTF_8);
+    HashMap<String, Integer> words = new HashMap<String, Integer>();
+
+    declaration = declaration.replaceAll("[^[\\p{Alnum}\\-]]", "");
+
+    System.out.println(declaration);
   }
 
   public static void main(String[] args) {
